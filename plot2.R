99d34d730a1fd3ae55ff3658aa1a068a7e201ca0
seddonma <matthew.seddon@outlook.com>
2015-06-11 16:54:29 -0400

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,3 @@
+## Rather than include the download scrpit, the file was downloaded and unzipped manually, as per the assignment instructions.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
