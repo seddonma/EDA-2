4a735d92da893dda03d496eb26122777a77093cd
seddonma <matthew.seddon@outlook.com>
2015-06-12 11:42:46 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -7,9 +7,10 @@ library(plyr)
 NEI_BC <- subset(NEI, NEI$fips == "24510")
 tot_em_BC <- ddply(NEI_BC, .(year), summarise, total_emissions = sum(Emissions))
 
-## Creates a graph and save it as png file titled "plot2.png"
+## Creates a graph and trendline and save it as png file titled "plot2.png"
 png("plot2.png", width = 480, height = 480)
-plot(tot_em_BC$year, tot_em_BC$total_emissions, pch = 19, bg = "black", main = "Total emissions in the US for the years 1999, 2002, 2005, and 2008", xlab = "Year", ylab = "Total Emissions (PM2.5)")
-fit <- lm(total_emissions~year, tot_em)
+plot(tot_em_BC$year, tot_em_BC$total_emissions, pch = 19, bg = "black", xlab = "Year", ylab = "Total Emissions (PM2.5)")
+title(main = "Total emissions in Baltimore City for the years 1999, 2002, 2005, and 2008", cex.main = 1)
+fit <- lm(total_emissions~year, tot_em_BC)
 abline(fit, col = "blue")
 dev.off()
