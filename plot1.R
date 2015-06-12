343903203a40e82a1bcb0b0ef687300601054b7b
seddonma <matthew.seddon@outlook.com>
2015-06-12 09:47:57 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ SCC <- readRDS("Source_Classification_Code.rds")
 ## Creates a dataframe that shows total emissions for the sample years
 tot_em <- ddply(NEI, .(year), summarise, total_emissions = sum(Emissions))
 
-## Create graph and save it as png file titled "plot1.png"
+## Creates a graph and saves it as png file titled "plot1.png"
 png("plot1.png", width = 480, height = 480)
-plot(x$year, x$total_emissions, pch = 19, bg = "black", main = "Total emissions in the US for the years 1999, 2002, 2005, and 2008", xlab = "Year", ylab = "Total Emissions (PM2.5)")
+plot(tot_em$year, tot_em$total_emissions, pch = 19, bg = "black", main = "Total emissions in the US for the years 1999, 2002, 2005, and 2008", xlab = "Year", ylab = "Total Emissions (PM2.5)")
 dev.off()
