2763dcd9d5ad697100e1cb8746ff63e9eabda531
seddonma <matthew.seddon@outlook.com>
2015-06-12 11:32:10 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -8,7 +8,8 @@ tot_em <- ddply(NEI, .(year), summarise, total_emissions = sum(Emissions))
 
 ## Creates a graph and saves it as png file titled "plot1.png"
 png("plot1.png", width = 480, height = 480)
-plot(tot_em$year, tot_em$total_emissions, pch = 19, bg = "black", main = "Total emissions in the US for the years 1999, 2002, 2005, and 2008", xlab = "Year", ylab = "Total Emissions (PM2.5)")
+plot(tot_em$year, tot_em$total_emissions, pch = 19, bg = "black", xlab = "Year", ylab = "Total Emissions (PM2.5)")
+title(main = "Total emissions in the US for the years 1999, 2002, 2005, and 2008", cex.main = 1)
 fit <- lm(total_emissions~year, tot_em)
 abline(fit, col = "blue")
 dev.off()
